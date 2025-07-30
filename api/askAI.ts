0eb12fde1aa27b326991ad6e0359756af5d54454
AryanEjantkar <aryanvimalejantkar@gmail.com>
2025-07-30 15:45:32 +0530

    WIP: saving progress before rebase

--- api/askAI.ts
+++ api/askAI.ts
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
-  }
+    }
 
   const { question } = req.body;
   if (!question) {
